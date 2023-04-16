af5b00de29adff2ec9f01d6f288b075b6cb04da7
m11o <m11o@hey.jp>
2023-04-16 10:32:14 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,30 @@
 4. 閉じた後、commitする
 5. どっかでpushする
  */
+mod git;
+
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
-use std::process::{Command, Output};
-
+use std::process::{Command, ExitStatus, Output};
 use chrono::{Local, NaiveDate};
+
+use crate::git::Git;
 
 fn main() {
     let now = Local::now().date_naive();
-    let file_name = buuld_file_name(&now);
+    let file_name = build_file_name(&now);
 
-    open_or_create_file_with_vim(&file_name);
+    let status = open_or_create_file_with_vim(&file_name);
+    if status.success() {
+        Git::add(&file_name);
+        Git::commit(&file_name);
+    } else {
+        println!("Failed to vim")
+    }
 }
 
-fn buuld_file_name<'a>(date: &'a NaiveDate) -> String {
+fn build_file_name(date: &NaiveDate) -> String {
     let mut file_name = "./notes/".to_string();
     file_name.push_str(&date.to_string());
     file_name.push_str(".md");
@@ -27,7 +36,7 @@ fn buuld_file_name<'a>(date: &'a NaiveDate) -> String {
     file_name
 }
 
-fn open_or_create_file_with_vim(file_name: &str) {
+fn open_or_create_file_with_vim(file_name: &str) -> ExitStatus {
     let path = Path::new(file_name);
 
     if !path.exists() {
@@ -41,34 +50,8 @@ fn open_or_create_file_with_vim(file_name: &str) {
     }
 
     // Vimでファイルを開く
-    let status = Command::new("nvim")
+    Command::new("nvim")
         .arg(file_name)
         .status()
-        .expect("Failed to start Vim process.");
-
-    // Vimの実行結果を待機する
-    if status.success() {
-        git_add(file_name).expect("Failed to git add for file");
-        git_commit("update").expect("Failed to commit");
-    } else {
-        println!("Vim closed with an error.");
-    }
+        .expect("Failed to start Vim process.")
 }
-
-fn git_add(file_name: &str) -> Result<Output, std::io::Error> {
-  let output = Command::new("git")
-      .arg("add")
-      .arg(file_name)
-      .output()?;
-    Ok(output)
-}
-
-fn git_commit(commit_message: &str) -> Result<Output, std::io::Error> {
-    let output = Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg(commit_message)
-        .output()?;
-    Ok(output)
-}
-
