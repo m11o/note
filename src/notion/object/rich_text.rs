6197e763d5627651539b688758f4c212127d5e6c
m11o <m11o@hey.jp>
2023-05-06 13:54:28 +0900

    add rich_text object

---
+++ src/notion/object/rich_text.rs
@@ -0,0 +1,36 @@
+use crate::notion::object::color::Color;
+
+pub enum RichTextType {
+    Text(RichTextText),
+    Mention(RichTextMention),
+    Equation(RichTextEquation),
+}
+
+struct RichTextText {
+    content: String,
+    link: Option<String>,
+}
+
+// 不要の可能性があるので、一旦未実装
+struct RichTextMention {}
+
+struct RichTextEquation {
+    expression: String,
+}
+
+struct Annotations {
+    bold: bool,
+    italic: bool,
+    strikethrough: bool,
+    underline: bool,
+    code: bool,
+    color: Color,
+}
+
+pub struct RichText<T> {
+    rich_text_type: RichTextType,
+    content: T,
+    plain_text: String,
+    href: Option<String>,
+    annotations: Annotations,
+}
